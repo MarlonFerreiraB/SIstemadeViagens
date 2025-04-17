77258a78d935c29a915266654d71a338e77c80c4
Marlon Ferreira <mferreirab03@gmail.com>
2025-04-17 14:51:19 -0300

    feat: adicionado novo arquivo de GerenciadorPassageiro.java(addPassageiro, procurarPassageiro).

---
+++ src/Services/GerenciadorPassageiro.java
@@ -0,0 +1,34 @@
+package Services;
+
+import Entities.Passageiro;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GerenciadorPassageiro {
+    Passageiro passageiro;
+    List<Passageiro> passageiroList = new ArrayList<>();
+
+    public boolean addPassageiro(String name, String cpf){
+        for(Passageiro p: passageiroList){
+            if(p.getCpf().equals(cpf)){
+                System.out.println("Ja existe um cadastro com esse cpf");
+                return false;
+            }
+        }
+        Passageiro novoPassageiro = new Passageiro(name,cpf);
+        passageiroList.add(novoPassageiro);
+        return true;
+    }
+
+    public Passageiro procurarPassageiro(String cpf){
+        for(Passageiro p: passageiroList){
+            if (p.getCpf().equals(cpf)) {
+                System.out.println("Passageiro encontrado");
+                return p;
+            }
+        }
+        System.out.println("Passageiro não encontrado");
+        return  null;
+    }
+}
