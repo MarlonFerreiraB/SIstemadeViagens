3fa475964e9fd8a41eca046d7996d0cc36766d94
Marlon Ferreira <mferreirab03@gmail.com>
2025-04-17 14:36:20 -0300

    feat: adicionado o corpo da class(Construor, get e toString)

--- src/Entities/Voo.java
+++ src/Entities/Voo.java
@@ -1,4 +1,29 @@
 package Entities;
 
+import java.util.Arrays;
+
 public class Voo {
+    private String destino;
+    private double preco;
+    private Passageiro[] passageiros = new Passageiro[30];
+
+    public Voo(String destino, double preco){
+        this.destino = destino;
+        this.preco = preco;
+    }
+
+    public Passageiro[] getPassageiros(){
+        return passageiros;
+    }
+    public String getDestino(){return destino;}
+    public double getPreco(){return preco;}
+
+    @Override
+    public String toString() {
+        return "Voo{" +
+                "destino='" + destino + '\'' +
+                ", preco=" + preco +
+                ", passageiros=" + Arrays.toString(passageiros) +
+                '}';
+    }
 }
