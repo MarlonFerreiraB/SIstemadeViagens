80f059cb302ba5587de7067ec14e49a6e8843be0
Marlon Ferreira <mferreirab03@gmail.com>
2025-04-17 14:28:53 -0300

    feat: adicionado o corpo da class(Construor, get e toString)

--- src/Entities/Passageiro.java
+++ src/Entities/Passageiro.java
@@ -1,4 +1,22 @@
 package Entities;
 
 public class Passageiro {
+    private String name;
+    private String cpf;
+
+    public Passageiro(String name, String cpf){
+        this.name =name;
+        this.cpf = cpf;
+    }
+
+    public String getName(){return name;}
+    public String getCpf(){return cpf;}
+
+    @Override
+    public String toString() {
+        return "Passageiro{" +
+                "name='" + name + '\'' +
+                ", cpf='" + cpf + '\'' +
+                '}';
+    }
 }
