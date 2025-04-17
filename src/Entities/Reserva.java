eac1213d0ad13e56a96bd1fe3b4c9423e85e8253
Marlon Ferreira <mferreirab03@gmail.com>
2025-04-17 14:41:11 -0300

    feat: adicionado o corpo da class(Construor, get e toString)

--- src/Entities/Reserva.java
+++ src/Entities/Reserva.java
@@ -1,4 +1,23 @@
 package Entities;
 
 public class Reserva {
+    private Passageiro passageiro;
+    private Voo voo;
+
+    public Reserva(Passageiro passageiro, Voo voo){
+        this.passageiro = passageiro;
+        this.voo = voo;
+    }
+    public Passageiro getPassageiro(){
+        return passageiro;
+    }
+    public Voo getVoo(){return voo;}
+
+    @Override
+    public String toString() {
+        return "Reserva{" +
+                "passageiro=" + passageiro +
+                ", voo=" + voo +
+                '}';
+    }
 }
